4d38415ea533955ed40153bf65981a8b3870bdc0
Kevin Cabral <kevin1tdsb@gmail.com>
2023-11-20 20:04:43 -0300

    conclusão do exercicio

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,33 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import PostComment from ".";
 
-describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
+describe("Teste para o componente PostComment", () => {
+  test("Deve renderizar o componente corretamente", () => {
+    render(<PostComment />);
+    expect(screen.getByText("Comentar")).toBeInTheDocument();
+  });
+
+  test("Deve adicionar 2 comentários", async () => {
+    render(<PostComment />);
+    fireEvent.change(screen.getByTestId("campo-comentario"), {
+      target: {
+        value: "Lorem ipsum",
+      },
     });
+    fireEvent.click(screen.getByTestId("btn-adicionar"));
+
+    await waitFor(() => {
+      expect(screen.getByText("Lorem ipsum")).toBeInTheDocument();
+    });
+    fireEvent.change(screen.getByTestId("campo-comentario"), {
+      target: {
+        value: "Dolor",
+      },
+    });
+    fireEvent.click(screen.getByTestId("btn-adicionar"));
+
+    await waitFor(() => {
+      expect(screen.getByText("Dolor")).toBeInTheDocument();
+    });
+  });
 });
